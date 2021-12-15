bb1024df7bccd88d9cbfe24340e3fd759f410b43
Shotray <695290109@qq.com>
2021-12-15 23:59:46 +0800

    Initial commit

---
+++ src/main/java/cn/edu/tongji/dwhivebackend/DwHiveBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.tongji.dwhivebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwHiveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DwHiveBackendApplication.class, args);
+    }
+
+}
